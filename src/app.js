43d0f12ac42236ddfe7bcc4eacb39a3c53453e7c
Sunnyvie-ctrl <minh-nhat.bui@epita.fr>
2026-02-21 14:55:44 +0100

    feat(user): register user routes in express application

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const userRoutes = require('./routes/user.routes');
+app.use('/users', userRoutes);
+
 app.use(express.json());
 
 module.exports = app;
